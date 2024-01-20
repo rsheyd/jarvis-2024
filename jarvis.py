29fe8a885b72eb28c812f36d095d5aed85225a27
Roman Sheydvasser <s.roman@gmail.com>
2024-01-19 20:53:29 -0500

    add venv and update to use openai

--- jarvis.py
+++ jarvis.py
@@ -1,23 +1,34 @@
 import time
+from openai import OpenAI
+
+client = OpenAI()
 
 def main():
     human = 'Roman'
-    prompt_for_input = 'Hello '+human+'. How are you?\n'
-    instruction = parse(wait_for_input(prompt_for_input))
+    prompt_for_input = '> Hello '+human+'. How are you?\n'
+    messages = []
+    instruction, messages = parse(wait_for_input(prompt_for_input), messages)
     while instruction != 'bye':
         prompt_for_input = instruction
-        instruction = parse(wait_for_input(prompt_for_input))
-    print('Bye '+human)
+        instruction, messages = parse(wait_for_input(prompt_for_input), messages)
+    print('> Bye '+human)
     exit()
-    
+
 
 def wait_for_input(prompt_for_input):
-    human_input = input(prompt_for_input)
+    human_input = input(prompt_for_input+"# ")
     return human_input
 
-def parse(instruction):
+def parse(instruction, messages):
     if instruction == 'bye':
-        return 'bye'
-    return "That's interesting. Tell me more.\n"
+        return 'bye', []
+    messages.append({"role": "user", "content": instruction})
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo", messages=messages
+    )  
+    answer = completion.choices[0].message.content
+    print('> '+answer)
+    messages.append({"role": "assistant", "content": answer})
+    return '', messages
 
 main()
